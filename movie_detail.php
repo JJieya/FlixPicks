144080cdde964b786ed0ffcb08544c906920cd2e
Jieya <115391084+JJieya@users.noreply.github.com>
2024-05-23 03:24:00 +1000

    Update movie_detail.php

--- movie_detail.php
+++ movie_detail.php
@@ -58,7 +58,7 @@ $conn->close();
     <style>
         .container {
             margin-top: 100px;
-            padding:20px;
+            padding:40px;
             max-width: 900px;
             background-color:white;
             margin-bottom: 100px;
@@ -72,34 +72,141 @@ $conn->close();
             margin-bottom: 20px;
             color: green;
         }
-        .ratings table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-        .ratings th,
-        .ratings td {
-            padding: 8px;
-            border-bottom: 1px solid #dee2e6;
-            text-align: left;
-        }
-        .ratings th {
-            background-color: #f8f9fa;
-            color: #212529;
-            font-weight: bold;
-        }
-        .rate-btn-container {
-            text-align: center;
-            margin-top: 20px;
-            
-        }
-        .rate-btn {
-            padding: 10px 20px;
-            font-size: 16px;
-
-        }
+       
+  
         .nav-item.right {
             margin-left: auto;
         }
+
+
+        a {
+            text-decoration: none !important;
+            color: inherit;
+        }
+
+        a:hover {
+            color: #455A64;
+        }
+
+        .card {
+            border-radius: 5px;
+            background-color: #fff;
+            padding-left: 60px;
+            padding-right: 60px;
+            margin-top: 30px;
+            padding-top: 30px;
+            padding-bottom: 30px;
+        }
+
+        .rating-box {
+            width: 130px;
+            height: 130px;
+            margin-right: auto;
+            margin-left: auto;
+            background-color: #FBC02D;
+            color: #fff;
+        }
+
+        .rating-label {
+            font-weight: bold;
+        }
+
+        /* Rating bar width */
+        .rating-bar {
+            width: 300px;
+            padding: 8px;
+            border-radius: 5px;
+        }
+
+        /* The bar container */
+        .bar-container {
+        width: 100%;
+        background-color: #f1f1f1;
+        text-align: center;
+        color: white;
+        border-radius: 20px;
+        cursor: pointer;
+        margin-bottom: 5px;
+        }
+
+        /* Individual bars */
+        .bar-5 {
+            width: 70%;
+            height: 13px;
+            background-color: #FBC02D; 
+            border-radius: 20px;
+
+        }
+        .bar-4 {
+            width: 30%;
+            height: 13px;
+            background-color: #FBC02D; 
+            border-radius: 20px;
+
+        }
+        .bar-3 {
+            width: 20%;
+            height: 13px;
+            background-color: #FBC02D; 
+            border-radius: 20px;
+
+        }
+        .bar-2 {
+            width: 10%;
+            height: 13px;
+            background-color: #FBC02D; 
+            border-radius: 20px;
+
+        }
+        .bar-1 {
+            width: 0%;
+            height: 13px;
+            background-color: #FBC02D; 
+            border-radius: 20px;
+
+        }
+
+        .star-active {
+            color: #FBC02D;
+            margin-top: 10px;
+            margin-bottom: 10px;
+        }
+
+        .star-active:hover {
+            color: #F9A825;
+            cursor: pointer;
+        }
+
+        .star-inactive {
+            color: #CFD8DC;
+            margin-top: 10px;
+            margin-bottom: 10px;
+        }
+
+        .blue-text {
+            color: #0091EA;
+        }
+
+        .content {
+            font-size: 18px;
+        }
+
+        .profile-pic {
+            width: 60px;
+            height: 60px;
+            border-radius: 100%;
+            margin-right: 30px;
+        }
+        
+        .pic {
+            width: 80px;
+            height: 80px;
+            margin-right: 10px;
+        }
+
+        .vote {
+            cursor: pointer;
+        }  
     </style>
 </head>
 <body>
@@ -132,7 +239,7 @@ $conn->close();
 
     <!-- Bootstrap template from free resource and modified by my own design -->
     <section class="container">
-            <div class="container px-4 px-lg-5 my-5">
+           
                 <div class="row gx-4 gx-lg-5 align-items-center">
                     <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="<?php echo htmlspecialchars($movie['poster_url']); ?>" alt="<?php echo htmlspecialchars($movie['name']); ?>" /></div>
                     <div class="col-md-6">
@@ -145,36 +252,40 @@ $conn->close();
                         </div>
                         <p class="lead"><?php echo htmlspecialchars($movie['synopsis']); ?></p>
                         <div class="d-flex">
-            
                             <button class="btn btn-outline-dark flex-shrink-0" type="button"
                                 onclick="checkLogin(<?php echo $movie_id; ?>)">Rate This Movie
                             </button>
                         </div>
                     </div>
-                </div>
             </div>
             <div class="ratings">
                 <h2>Ratings and Reviews</h2>
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>User</th>
-                            <th>Rating</th>
-                            <th>Review</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($ratings as $rating) : ?>
-                            <tr>
-                                <td><?php echo htmlspecialchars($rating['user_name']); ?></td>
-                                <td><?php echo htmlspecialchars($rating['star_rating']); ?> Stars</td>
-                                <td><?php echo htmlspecialchars($rating['review_comment']); ?></td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
             </div>
-
+            <div class="ratings">
+                    <?php foreach ($ratings as $rating) : ?>		
+                        <div class="card">
+                            <div class="row d-flex">
+                                <div>
+                                    <img class="profile-pic" src="person-circle.svg">
+                                </div>
+                                <div class="d-flex flex-column">
+                                    <h3 class="mt-2 mb-0"><?php echo htmlspecialchars($rating['user_name']); ?></h3>
+                                    <div>
+                                        <p class="text-left"><span class="text-mutedpt"><?php echo htmlspecialchars($rating['star_rating']); ?> Stars</span>
+                                        <span class="fa fa-star star-active ml-3"></span>
+                                        <span class="fa fa-star star-active"></span>
+                                        <span class="fa fa-star star-active"></span>
+                                        <span class="fa fa-star star-active"></span>
+                                        <span class="fa fa-star star-inactive"></span></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row text-left">
+                                <p class="content"><?php echo htmlspecialchars($rating['review_comment']); ?> </p>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>    
+            </div>
     </section>
 
    <!-- <div class="container">
