5ac1f4e9a4885a19a988ecedec2bc035f7e31637
Jieya <115391084+JJieya@users.noreply.github.com>
2024-05-26 19:57:39 +1000

    Update logout.php

--- logout.php
+++ logout.php
@@ -8,7 +8,7 @@ $_SESSION = array();
 // Destroy the session.
 session_destroy();
  
-// Redirect to login page
-header("location: login.php");
+// Redirect to homepage page
+header("location: homepage.php");
 exit;
 ?>
