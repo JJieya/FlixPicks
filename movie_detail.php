aa8b1380b271714781c8a997d10384216fc36d89
Jieya <115391084+JJieya@users.noreply.github.com>
2024-06-01 19:41:28 +1000

    Update movie_detail.php
    Cleaned up redundant code.
    Added some comments to the function.

--- movie_detail.php
+++ movie_detail.php
@@ -18,20 +18,23 @@ if (isset($_GET['id'])) {
         exit;
     }
 
-    // Retrieve ratings for this movie
+    // SQL query to retrieve ratings for a specific movie along with user names
     $sql_ratings = "
-        SELECT r.*, u.name AS user_name
-        FROM rating r
-        JOIN users u ON r.user_id = u.id
-        WHERE r.movie_id = ?
+        SELECT rating.*, users.name AS user_name
+        FROM rating 
+        JOIN users ON rating.user_id = users.id
+        WHERE rating.movie_id = ?
     ";
+    // Prepare the SQL statement
     $stmt_ratings = $conn->prepare($sql_ratings);
+    // Bind the movie_id parameter to the SQL statement
     $stmt_ratings->bind_param("i", $movie_id);
     $stmt_ratings->execute();
     $result_ratings = $stmt_ratings->get_result();
-
+    // Initialize an empty array to store the ratings
     $ratings = [];
     if ($result_ratings->num_rows > 0) {
+        // Loop through each row in the result set
         while ($row = $result_ratings->fetch_assoc()) {
             $ratings[] = $row;
         }
@@ -73,7 +76,6 @@ $conn->close();
             color: green;
         }
        
-  
         .nav-item.right {
             margin-left: auto;
         }
@@ -93,95 +95,6 @@ $conn->close();
             padding-bottom: 30px;
         }
 
-        .rating-box {
-            width: 130px;
-            height: 130px;
-            margin-right: auto;
-            margin-left: auto;
-            background-color: #FBC02D;
-            color: #fff;
-        }
-
-        .rating-label {
-            font-weight: bold;
-        }
-
-        /* Rating bar width */
-        .rating-bar {
-            width: 300px;
-            padding: 8px;
-            border-radius: 5px;
-        }
-
-        /* The bar container */
-        .bar-container {
-        width: 100%;
-        background-color: #f1f1f1;
-        text-align: center;
-        color: white;
-        border-radius: 20px;
-        cursor: pointer;
-        margin-bottom: 5px;
-        }
-
-        /* Individual bars */
-        .bar-5 {
-            width: 70%;
-            height: 13px;
-            background-color: #FBC02D; 
-            border-radius: 20px;
-
-        }
-        .bar-4 {
-            width: 30%;
-            height: 13px;
-            background-color: #FBC02D; 
-            border-radius: 20px;
-
-        }
-        .bar-3 {
-            width: 20%;
-            height: 13px;
-            background-color: #FBC02D; 
-            border-radius: 20px;
-
-        }
-        .bar-2 {
-            width: 10%;
-            height: 13px;
-            background-color: #FBC02D; 
-            border-radius: 20px;
-
-        }
-        .bar-1 {
-            width: 0%;
-            height: 13px;
-            background-color: #FBC02D; 
-            border-radius: 20px;
-
-        }
-
-        .star-active {
-            color: #FBC02D;
-            margin-top: 10px;
-            margin-bottom: 10px;
-        }
-
-        .star-active:hover {
-            color: #F9A825;
-            cursor: pointer;
-        }
-
-        .star-inactive {
-            color: #CFD8DC;
-            margin-top: 10px;
-            margin-bottom: 10px;
-        }
-
-        .blue-text {
-            color: #0091EA;
-        }
-
         .content {
             font-size: 18px;
         }
@@ -199,9 +112,6 @@ $conn->close();
             margin-right: 10px;
         }
 
-        .vote {
-            cursor: pointer;
-        }  
     </style>
 </head>
 <body>
@@ -224,7 +134,7 @@ $conn->close();
             </li>
             <li class="nav-item right">
                 <?php if (isset($_SESSION['user_id'])): ?>
-                    <a class="nav-link" href="logout.php">LOGOUT (<?php echo htmlspecialchars($_SESSION['username']); ?>)</a>
+                    <a class="nav-link" href="logout.php">LOGOUT (<?php echo $_SESSION['username']; ?>)</a>
                 <?php else: ?>
                     <a class="nav-link" href="login.php">LOGIN</a>
                 <?php endif; ?>
@@ -233,25 +143,26 @@ $conn->close();
     </nav>
 
     <!-- Bootstrap template from free resource and modified by my own design -->
-    <section class="container">
-           
-                <div class="row gx-4 gx-lg-5 align-items-center">
-                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="<?php echo htmlspecialchars($movie['poster_url']); ?>" alt="<?php echo htmlspecialchars($movie['name']); ?>" /></div>
-                    <div class="col-md-6">
-                        <h1 class="display-5 fw-bolder"><?php echo htmlspecialchars($movie['name']); ?></h1>
-                        <div class="fs-5 mb-5">
-                            <span class="text-decoration-line-through">Directed by: <?php echo htmlspecialchars($movie['director']); ?></span>
-                        </div>
-                        <div class="fs-5 mb-5">
-                            <span class="text-decoration-line-through">Released in: <?php echo htmlspecialchars($movie['release_year']); ?></span>
-                        </div>
-                        <p class="lead"><?php echo htmlspecialchars($movie['synopsis']); ?></p>
-                        <div class="d-flex">
-                            <button class="btn btn-outline-dark flex-shrink-0" type="button"
-                                onclick="checkLogin(<?php echo $movie_id; ?>)">Rate This Movie
-                            </button>
-                        </div>
+    <section class="container">       
+            <div class="row gx-4 gx-lg-5 align-items-center">
+                <div class="col-md-6">
+                    <img class="card-img-top mb-5 mb-md-0" src="<?php echo $movie['poster_url']; ?>" alt="<?php echo $movie['name']; ?>" />
+                </div>
+                <div class="col-md-6">
+                    <h1 class="display-5 fw-bolder"><?php echo $movie['name']; ?></h1>
+                    <div class="fs-5 mb-5">
+                        <span class="text-decoration-line-through">Directed by: <?php echo $movie['director']; ?></span>
                     </div>
+                    <div class="fs-5 mb-5">
+                        <span class="text-decoration-line-through">Released in: <?php echo $movie['release_year']; ?></span>
+                    </div>
+                    <p class="lead"><?php echo $movie['synopsis']; ?></p>
+                    <div class="d-flex">
+                        <button class="btn btn-outline-dark flex-shrink-0" type="button"
+                            onclick="checkLogin(<?php echo $movie_id; ?>)">Rate This Movie
+                        </button>
+                    </div>
+                </div>
             </div>
             <div class="ratings">
                 <h2>Ratings and Reviews</h2>
@@ -264,19 +175,14 @@ $conn->close();
                                 <img class="profile-pic" src="person-circle.svg">
                             </div>
                             <div class="d-flex flex-column">
-                                <h3 class="mt-2 mb-0"><?php echo htmlspecialchars($rating['user_name']); ?></h3>
+                                <h3 class="mt-2 mb-0"><?php echo $rating['user_name']; ?></h3>
                                 <div>
-                                    <p class="text-left"><span class="text-mutedpt"><?php echo htmlspecialchars($rating['star_rating']); ?> Stars</span>
-                                    <span class="fa fa-star star-active ml-3"></span>
-                                    <span class="fa fa-star star-active"></span>
-                                    <span class="fa fa-star star-active"></span>
-                                    <span class="fa fa-star star-active"></span>
-                                    <span class="fa fa-star star-inactive"></span></p>
+                                    <p class="text-left"><span class="text-mutedpt"><?php echo $rating['star_rating']; ?> Stars</span></p>
                                 </div>
                             </div>
                         </div>
                         <div class="row text-left">
-                            <p class="content"><?php echo htmlspecialchars($rating['review_comment']); ?> </p>
+                            <p class="content"><?php echo $rating['review_comment']; ?> </p>
                         </div>
                     </div>
                 <?php endforeach; ?>    
@@ -291,11 +197,15 @@ $conn->close();
 
     <script>
         function checkLogin(movie_id) {
+            // Check if the user is logged in by verifying if a session variable 'user_id' is set
             <?php if (!isset($_SESSION['user_id'])): ?>
-                alert("Please log in to rate this movie.");
+                // If the user is not logged in, display an alert message
+                alert("Please log in or register to rate this movie.");
+                // link to login page 
                 window.location.href = 'login.php';
                 return false;
             <?php else: ?>
+                // If the user is logged in, then link to the rate_movie.php with the movie ID
                 window.location.href = 'rate_movie.php?id=' + movie_id;
                 return true;
             <?php endif; ?>
